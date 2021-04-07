9461ce578ce74c290cbb37ac9141691a36bb17b7
dino2306 <ducloi23620@gmail.com>
2021-04-07 17:41:09 +0700

    hello
    hello

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,7 +12,7 @@ public class PlayerController : MonoBehaviour
     private Collider2D coll;
 
     //FSM
-    private enum State {idle, running, jumping, falling } // các hành động của nhân vâtj
+    private enum State {idle, running, jumping, falling, hurt } // các hành động của nhân vâtj
     private State state = State.idle;
 
     
@@ -22,7 +22,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float speed = 5f;
     [SerializeField] private float jumpForce = 10f;
     [SerializeField] private int cherries = 0;
-    [SerializeField] private Text cherryText;   
+    [SerializeField] private Text cherryText;
+    [SerializeField] private float hurtForce = 10f;
 
 
     private void Start()
@@ -34,8 +35,12 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        MoveManager();
-        VelocityState(); 
+        if (state != State.hurt )
+        {
+            MoveManager();
+        }
+        
+        AnimationState(); 
         anim.SetInteger("state", (int)state); //đặt animation dựa trên trạng thái của Enumerator
     }
 
@@ -69,12 +74,17 @@ public class PlayerController : MonoBehaviour
         // nhảy bằng phím space or chạm vào màn hình
         if (Input.GetButtonDown("Jump") && coll.IsTouchingLayers())
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-            state = State.jumping;
+            Jump();
         }   
     }
 
-    private void VelocityState()
+    private void Jump() 
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        state = State.jumping;
+    }
+
+    private void AnimationState()
     {
         if(state == State.jumping) 
         { 
@@ -91,6 +101,16 @@ public class PlayerController : MonoBehaviour
                 state = State.idle;
             }
         }
+
+        else if (state == State.hurt)
+        {
+            if(Mathf.Abs(rb.velocity.x) < .1f)
+            {
+                state = State.idle;
+
+            }
+        }
+
         // trạng thái của nhân vật
         else if(Mathf.Abs(rb.velocity.x) > 2f)
         {
@@ -103,7 +123,7 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-
+    // va chạm ăn điểm
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Collectable")
@@ -112,6 +132,35 @@ public class PlayerController : MonoBehaviour
             cherries += 1;
             cherryText.text = cherries.ToString();
         }    
+    }
+    
+    
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Enemy")
+        {
+            Frog frog = other.gameObject.GetComponent<Frog>();
+            if(state == State.falling)
+            {
+                frog.JumpedOn();
+                Jump(); 
+            }
+            else
+            {
+                state = State.hurt;
+                if(other.gameObject.transform.position.x > transform.position.x)
+                {
+                    //Enemy is to my right therefore I should be damaged and move left
+                    rb.velocity = new Vector2(-hurtForce, rb.velocity.y);
+                }
+                else 
+                {
+                    //Enemy is to my right therefore I should be damaged and move left
+                    rb.velocity = new Vector2(hurtForce, rb.velocity.y);
+                }
+            }
+            
+        }
     }
 
 
